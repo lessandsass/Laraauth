a9be76ef796abc6a5e0da5bd2aecb2124046c061
Less <31947204+lessandsass@users.noreply.github.com>
2020-10-31 16:24:21 +0530

    updated, tailwindcss not added by cdn, added by npm install

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,20 +3,15 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-4/12 bg-white p-6 rounded-lg">
-
-            <form action="{{ route('register') }}" method="post">
-                @csrf
-                <div class="mb-4">
-                    <label for="name" class="sr-only">Name</label>
-                    <input type="text" name="name" id="name" placeholder="Your name" class="bg-gray-100 border-2 w-full p-4 rounded-lg outline-none @error('name') border-red-500 @enderror" autocomplete="off" value="{{ old('name') }}">
-
-                    @error('name')
-                        <div class="text-red-500 mt-2 text-sm">
-                            {{ $message }}
-                        </div>
-                    @enderror
+            @if (session('status'))
+                <div class="bg-red-500 p-4 rounded-lg mb-6 text-white text-center">
+                    {{ session('status') }}
                 </div>
+            @endif
 
+            <form action="{{ route('login') }}" method="post">
+                @csrf
+               
                 <div class="mb-4">
                     <label for="email" class="sr-only">Email</label>
                     <input type="text" name="email" id="email" placeholder="Your email address" class="bg-gray-100 border-2 w-full p-4 rounded-lg outline-none @error('email') border-red-500 @enderror" autocomplete="off" value="{{ old('email') }}">
@@ -40,12 +35,14 @@
                 </div>
 
                 <div class="mb-4">
-                    <label for="password_confirmation" class="sr-only">Password again</label>
-                    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Password again" class="bg-gray-100 border-2 w-full p-4 rounded-lg outline-none @error('password_confirmation') border-red-500 @enderror">
+                    <div class="flex items-center">
+                        <input type="checkbox" name="remember" id="remember" class="mr-2">
+                        <label for="remember">Remember me</label>
+                    </div>
                 </div>
 
                 <div>
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full outline-none">Register</button>
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full outline-none">Login</button>
                 </div>
 
             </form>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,17 +10,32 @@
 <body class="bg-gray-100">
     
     <nav class="p-6 bg-white flex justify-between">
+        
         <ul class="flex items-center">
             <li><a href="" class="p-3">Home</a></li>
             <li><a href="{{ route('dashboard') }}" class="p-3">Dashboard</a></li>
             
         </ul>
+
         <ul class="flex items-center">
-            <li><a href="" class="p-3">Login</a></li>
-            <li><a href="" class="p-3">Register</a></li>
-            <li><a href="" class="p-3">Velnaygam Vipurthanan</a></li>
-            <li><a href="" class="p-3">Logout</a></li>
+
+            @auth
+                <li><a href="#" class="p-3">{{ auth()->user()->name }}</a></li>
+                <li>
+                    <form action="{{ route('logout') }}" method="post" class="p-3 outline-non">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
+                </li>
+            @endauth
+
+            @guest
+                <li><a href="{{ route('login') }}" class="p-3">Login</a></li>
+                
+                <li><a href="{{ route('register') }}" class="p-3">Register</a></li>
+            @endguest
         </ul>
+
     </nav>
 
     <div class="container mx-auto mt-6 px-6">
